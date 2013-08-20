e356a6fdacbc8ea8495d31cf83268dc0f2022027
poemdexter <poemdexter@gmail.com>
2013-08-19 22:55:00 -0500

    jump physics in.  about to say fuck it all and go to pure raycasting.

--- Assets/scripts/Movement.cs
+++ Assets/scripts/Movement.cs
@@ -5,26 +5,55 @@ public class Movement : MonoBehaviour
 {
 	public float speed = 5.0f;
 	public float jumpSpeed = 500.0f;
-	public float airControl = 0.5f;
-	public Vector3 jumpDirection = Vector3.zero;
 	public Vector3 moveDirection = Vector3.zero;
 	public bool isGrounded = false;
 	public bool isJumping = false;
 	public bool inAir = false;
+	public ContactPoint contact;
+	
+	void FixedUpdate() 
+	{
+		if (!isGrounded)
+		{
+			// check if grounded by raycasting down half the size of the collider height
+			if (Physics.Raycast(transform.position, -transform.up, this.collider.bounds.size.y/2))
+			{
+				isGrounded = true;
+				isJumping = false;
+				inAir = false;
+			}
+			else if (!inAir)
+				inAir = true;
+		}
+		
+		Move(); 
+	}
 	
 	void Move()
 	{
+		// moving left or right
 		if (Input.GetAxis("Horizontal") != 0)
-		{
 			moveDirection = new Vector3(Input.GetAxisRaw("Horizontal"), moveDirection.y, 0);
-			this.transform.Translate(moveDirection.normalized * speed * Time.deltaTime);
+		
+		// jumping while grounded
+		if (Input.GetButton("Jump") && isGrounded)
+		{
+			isJumping = true;
+			rigidbody.AddForce((transform.up) * jumpSpeed);
 		}
 		
-		if (Input.GetButtonDown("Jump") && isGrounded)
-		{
-			
-		}
+		this.transform.Translate(moveDirection.normalized * speed * Time.deltaTime);
 	}
 	
-	void FixedUpdate() { Move(); }
+	void OnCollisionStay(Collision collisionInfo)
+	{
+		contact = collisionInfo.contacts[0];
+		if (inAir || isJumping)
+			rigidbody.AddForceAtPosition(-rigidbody.velocity, contact.point); 
+	}
+	
+	void OnCollisionExit(Collision collisionInfo)
+	{
+		isGrounded = false; // left the collision of the ground
+	}
 }
